48f1d78f458051e05ec7b945ab2b1007df8f977c
ozansari89 <ozansari89@gmail.com>
2022-03-02 00:24:23 +0300

    Create app.ts

---
+++ app.ts
@@ -0,0 +1,7 @@
+import { Calendar } from '@syncfusion/ej2-calendars';
+//creates a calendar with weekNumber enabled
+let calendarObject: Calendar = new Calendar({
+    //sets the weekNumber
+    weekNumber: true
+});
+calendarObject.appendTo('#element');
